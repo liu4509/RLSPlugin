1ebbc8505211bf185713eb1074cdc727d3ee8859
liu4509 <2608606700@qq.com>
2023-04-29 15:47:30 +0800

    快捷键切换

--- content.ts
+++ content.ts
@@ -1,19 +1,50 @@
 import type { PlasmoCSConfig } from "plasmo"
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://riderx-l3.jing-chi.com/*"]
+  matches: ["https://riderx-l3b.jing-chi.com/*"]
+  // matches: ["https://riderx-l3.jing-chi.com/*"]
 }
 // 触发事件
 document.addEventListener("keypress", keyboard)
+function paiZiFun(paiZhi) {
+  paiZhi[0].dispatchEvent(boxClick)
+  paiZhi[0].dispatchEvent(boxClick)
+}
+function A(key, box) {
+  // 判断是否是绑定的键
+  if (key == "g") {
+    // 事件派发
+    console.log(key)
+    box[0].dispatchEvent(boxClick)
+  }
+  if (key == "h") {
+    // 事件派发
+    console.log(key)
+    box[1].dispatchEvent(boxClick)
+  }
+}
 // 回调
-function keyboard(eve) {
+async function keyboard(eve) {
   // 提取点击的键
   let x = eve.keyCode || eve.which
   let key = String.fromCharCode(x)
-  // 判断是否是绑定的键
-  if (key == "f") {
-    // console.log("f");
-    // let lab = document.querySelector(".ant-checkbox-input")
-    // lab.click()
+  //
+  // const newPaiZi = new Promise((resolve, reject) => {
+  //   paiZiFun(paiziClick)
+  // })
+  let paiziClick = document.querySelectorAll(
+    ".src-components-AppBar-AppBarIcon-module__appbar_icon_container--2RT8u"
+  )
+  let box = document.querySelectorAll(".ant-checkbox-input")
+
+  if (box[1] == undefined) {
+    await paiZiFun(paiziClick)
   }
+
+  await A(key, box)
 }
+var boxClick = new MouseEvent("click", {
+  bubbles: true,
+  cancelable: true,
+  view: window
+})
